7185293c230475545896321cb88ed8ad8f7addb5
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-11-26 22:27:16 +0100

    Create login-signup.js

---
+++ login-signup.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+
+const AuthPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // Perform login API call
+    try {
+      const response = await fetch('https://home-pal.onrender.com/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      // Handle successful login, e.g., redirect to another page
+      console.log('Login successful');
+      history.push('/homepage.html');
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    // Perform signup API call
+    try {
+      const response = await fetch('https://home-pal.onrender.com/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Signup failed');
+      }
+
+      // Handle successful signup, e.g., redirect to another page
+      console.log('Signup successful');
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="login-email">Email:</label>
+        <input
+          type="text"
+          id="login-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label htmlFor="login-password">Password:</label>
+        <input
+          type="password"
+          id="login-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit" id="submit">Login</button>
+      </form>
+
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSignup}>
+        <label htmlFor="login-email">Email:</label>
+        <input
+          type="text"
+          id="login-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label htmlFor="login-password">Password:</label>
+        <input
+          type="password"
+          id="login-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit" id="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
